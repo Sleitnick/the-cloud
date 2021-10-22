26dc570c17ad03026fc9f5850813ad5a89840e0d
Stephen Leitnick <sleitnick@gmail.com>
2021-10-22 10:10:54 -0400

    Add version type

--- publish_place.py
+++ publish_place.py
@@ -11,24 +11,24 @@ universe_id = sys.argv[2] # 123456789
 place_id = sys.argv[3] # 7795117711
 auth_key = sys.argv[4] # abcdefg
 
-url = f"https://apis.roblox.com/universes/v1/{universe_id}/places/{place_id}/versions"
+url = f"https://apis.roblox.com/universes/v1/{universe_id}/places/{place_id}/versions?versionType=Published"
 
 def main():
 	with open(filename, 'rb') as place:
 		data = place.read()
-	headers = {
-		'Content-Type': 'application/octet-stream',
-		'x-api-key': auth_key
-	}
-	res = requests.post(
-		url,
-		headers=headers,
-		data=data
-	)
-	res.raise_for_status()
-	res_data = res.json() # e.g. {"versionNumber":1}
-	with open(Path(filename).with_suffix('.json'), 'w') as place_response:
-		place_response.write(json.dumps(res_data, indent=2))
+		headers = {
+			'x-api-key': auth_key,
+			'Content-Type': 'application/octet-stream',
+		}
+		res = requests.post(
+			url,
+			headers=headers,
+			data=data
+		)
+		res.raise_for_status()
+		res_data = res.json() # e.g. {"versionNumber":1}
+		with open(Path(filename).with_suffix('.json'), 'w') as place_response:
+			place_response.write(json.dumps(res_data, indent=2))
 
 if __name__ == "__main__":
 	main()
