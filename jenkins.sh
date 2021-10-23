f1e82b6207f109c2137b4850b3112520509ee768
Stephen Leitnick <sleitnick@gmail.com>
2021-10-22 23:15:59 -0400

    Jenkins change

--- jenkins.sh
+++ jenkins.sh
@@ -3,4 +3,4 @@ docker run --name jenkins-server-test --rm --detach \
   --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
   --volume jenkins-data:/var/jenkins_home \
   --volume jenkins-docker-certs:/certs/client:ro \
-  --publish 8080:8080 --publish 50000:50000 jenkins-test
+  --publish 8080:8080 --publish 50000:50000 jenkins-server
